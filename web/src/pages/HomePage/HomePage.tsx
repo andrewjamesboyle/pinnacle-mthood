4b3aea966cfa3db471bed08a0877c646a70242e4
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-06-16 10:08:11 -0700

    state legislative services section built

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,19 +1,235 @@
-import { Link, routes } from '@redwoodjs/router'
+import { useState } from 'react'
+
+import { Dialog } from '@headlessui/react'
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from '@heroicons/react/20/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
 import { MetaTags } from '@redwoodjs/web'
 
+const features = [
+  {
+    name: 'Professional Government Relations',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'Superior Legislative Lobbying',
+    description:
+      'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Key Relationships and Coalition Building',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ServerIcon,
+  },
+]
+
+const navigation = [
+  { name: 'Services', href: '#' },
+  { name: 'About', href: '#' },
+  { name: 'Team', href: '#' },
+  { name: 'Contact', href: '#' },
+]
+
 const HomePage = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
+      <div className="bg-gray-900">
+        <header className="relative inset-x-0 top-0 z-50 bg-white">
+          <nav
+            className="flex items-center justify-between p-6 lg:px-8"
+            aria-label="Global"
+          >
+            <div className="flex lg:flex-1">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=gray&shade=800"
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="flex lg:hidden">
+              <button
+                type="button"
+                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="hidden lg:flex lg:gap-x-12">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="text-sm font-semibold leading-6 text-gray-800"
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+            {/* <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-white"
+              >
+                Log in <span aria-hidden="true">&rarr;</span>
+              </a>
+            </div> */}
+          </nav>
+          <Dialog
+            as="div"
+            className="lg:hidden"
+            open={mobileMenuOpen}
+            onClose={setMobileMenuOpen}
+          >
+            <div className="fixed inset-0 z-50" />
+            <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+              <div className="flex items-center justify-between">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">Your Company</span>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt=""
+                  />
+                </a>
+                <button
+                  type="button"
+                  className="-m-2.5 rounded-md p-2.5 text-gray-400"
+                  onClick={() => setMobileMenuOpen(false)}
+                >
+                  <span className="sr-only">Close menu</span>
+                  <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+              <div className="mt-6 flow-root">
+                <div className="-my-6 divide-y divide-gray-500/25">
+                  <div className="space-y-2 py-6">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                  <div className="py-6">
+                    <a
+                      href="#"
+                      className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                    >
+                      Log in
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </Dialog.Panel>
+          </Dialog>
+        </header>
+
+        <div className="relative isolate overflow-hidden pt-14">
+          <img
+            src="/plants.jpg"
+            alt=""
+            className="absolute inset-0 -z-10 h-full w-full object-cover"
+          />
+
+          <div className="lg:py-35 mx-auto max-w-2xl py-20 sm:py-32">
+            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
+                Announcing our newest branch.{' '}
+                <a href="#" className="font-semibold text-white">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="max-w-3xl text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                Pinnacle Advocacy Mount Hood
+              </h1>
+              <p className="mt-6 text-lg leading-8 text-gray-300">
+                Pinnacle Advocacy embraces a philosophy of representation that
+                is built on absolute integrity with an unquestioned commitment
+                to success.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
+                >
+                  Get started
+                </a>
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-white"
+                >
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="overflow-hidden bg-white py-24 sm:py-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+                <div className="lg:pr-8 lg:pt-4">
+                  <div className="lg:max-w-lg">
+                    <h2 className="text-base font-semibold leading-7 text-indigo-600">
+                      Our Services
+                    </h2>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                      State Legislative Lobbying
+                    </p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                      Pinnacle’s ability to precisely and credibly tell our
+                      clients’ story while finding innovative ways to advance
+                      policy interests has earned us a well-respected reputation
+                      with California’s policy makers in the State Capitol.
+                    </p>
+                    <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                      {features.map((feature) => (
+                        <div key={feature.name} className="relative pl-9">
+                          <dt className="inline font-semibold text-gray-900">
+                            <feature.icon
+                              className="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                              aria-hidden="true"
+                            />
+                            {feature.name}
+                          </dt>{' '}
+                          <dd className="inline">{feature.description}</dd>
+                        </div>
+                      ))}
+                    </dl>
+                  </div>
+                </div>
+                <img
+                  src="/mthood.jpg"
+                  alt="Product screenshot"
+                  className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+                  width={2432}
+                  height={1442}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
