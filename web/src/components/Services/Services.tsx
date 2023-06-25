a7802d593bc6695a5d07624cd2acc21e76dfcf38
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-06-25 12:48:01 -0700

    third section added to services

--- web/src/components/Services/Services.tsx
+++ web/src/components/Services/Services.tsx
@@ -54,6 +54,31 @@ const featuresTwo = [
   },
 ]
 
+const featuresThree = [
+  {
+    name: 'Intelligence gathering on procurement actions',
+    description:
+      'Commodo nec sagittis tortor mauris sed. Turpis tortor quis scelerisque diam id accumsan nullam tempus. Pulvinar etiam lacus volutpat eu. Phasellus praesent ligula sit faucibus.',
+    href: '#',
+    icon: CloudArrowUpIcon,
+  },
+
+  {
+    name: 'Collaboration to improve policies and streamline processes',
+    description:
+      'Pellentesque enim a commodo malesuada turpis eleifend risus. Facilisis donec placerat sapien consequat tempor fermentum nibh.',
+    href: '#',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Key Relationships and Coalition Building',
+    description:
+      'Pellentesque sit elit congue ante nec amet. Dolor aenean curabitur viverra suspendisse iaculis eget. Nec mollis placerat ultricies euismod ut condimentum.',
+    href: '#',
+    icon: ArrowPathIcon,
+  },
+]
+
 export default function Services() {
   return (
     <>
@@ -134,6 +159,61 @@ export default function Services() {
             <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
               <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
                 {featuresTwo.map((feature) => (
+                  <div key={feature.name} className="flex flex-col">
+                    <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
+                      <feature.icon
+                        className="h-5 w-5 flex-none text-indigo-600"
+                        aria-hidden="true"
+                      />
+                      {feature.name}
+                    </dt>
+                    <dd className="mt-4 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                      <p className="flex-auto">{feature.description}</p>
+                      <p className="mt-6">
+                        <a
+                          href={feature.href}
+                          className="text-sm font-semibold leading-6 text-indigo-600"
+                        >
+                          Learn more <span aria-hidden="true"></span>
+                        </a>
+                      </p>
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+          <div className="relative overflow-hidden pt-16">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+              <img
+                src="/plants.jpg"
+                alt="App screenshot"
+                className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+                width={2432}
+                height={1442}
+              />
+              <div className="relative" aria-hidden="true">
+                <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[7%]" />
+              </div>
+            </div>
+          </div>
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto mt-20 max-w-2xl lg:mx-0 lg:text-left">
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Government Procurement
+              </p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+                Pinnacle Advocacy is a comprehensive government affairs services
+                firm, representing clients before all levels of state
+                government. Our team's substantive policy knowledge, deep
+                political expertise, and unmatched work ethic results in
+                consistently successful outcomes on the high-stakes and
+                challenging issues in the State of Oregon.
+              </p>
+            </div>
+            <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+              <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+                {featuresThree.map((feature) => (
                   <div key={feature.name} className="flex flex-col">
                     <dt className="flex items-center gap-x-3 text-base font-semibold leading-7 text-gray-900">
                       <feature.icon
