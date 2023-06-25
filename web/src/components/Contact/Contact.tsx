c7b3a7d398869cd28e6c03c6e4be7859adf49f55
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2023-06-25 11:41:23 -0700

    sendgrid API unauthorized

--- web/src/components/Contact/Contact.tsx
+++ web/src/components/Contact/Contact.tsx
@@ -4,7 +4,42 @@ import {
   PhoneIcon,
 } from '@heroicons/react/24/outline'
 
+import {
+  FieldError,
+  Form,
+  Submit,
+  SubmitHandler,
+  TextAreaField,
+  TextField,
+  useForm,
+} from '@redwoodjs/forms'
+
+interface FormValues {
+  input: string
+}
+
 const Contact = () => {
+  const formMethods = useForm({ mode: 'onBlur' })
+
+  const onSubmit: SubmitHandler<FormValues> = async (userData) => {
+    try {
+      console.log('userData front end', userData)
+      const response = await fetch('/.redwood/functions/email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      })
+      formMethods.reset()
+
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.error('Error', error)
+    }
+  }
+
   return (
     <div className="relative isolate bg-white">
       <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
@@ -56,11 +91,7 @@ const Contact = () => {
                     aria-hidden="true"
                   />
                 </dt>
-                <dd>
-                  545 Mavis Island
-                  <br />
-                  Chicago, IL 99191
-                </dd>
+                <dd>Hood River, OR 97031</dd>
               </div>
               <div className="flex gap-x-4">
                 <dt className="flex-none">
@@ -90,16 +121,19 @@ const Contact = () => {
                 <dd>
                   <a
                     className="hover:text-gray-900"
-                    href="mailto:hello@example.com"
+                    href="mailto:tre@pinnacleadvocacy.com"
                   >
-                    hello@example.com
+                    tre@pinnacleadvocacy.com
                   </a>
                 </dd>
               </div>
             </dl>
           </div>
         </div>
-        <form
+        <Form
+          onSubmit={onSubmit}
+          config={{ mode: 'onBlur' }}
+          formMethods={formMethods}
           action="#"
           method="POST"
           className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-48"
@@ -108,36 +142,38 @@ const Contact = () => {
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
                 <label
-                  htmlFor="first-name"
+                  htmlFor="firstName"
                   className="block text-sm font-semibold leading-6 text-gray-900"
                 >
                   First name
                 </label>
                 <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
+                  <TextField
+                    name="firstName"
+                    id="firstName"
                     autoComplete="given-name"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    validation={{ required: true }}
                   />
+                  <FieldError name="firstName" />
                 </div>
               </div>
               <div>
                 <label
-                  htmlFor="last-name"
+                  htmlFor="lastName"
                   className="block text-sm font-semibold leading-6 text-gray-900"
                 >
                   Last name
                 </label>
                 <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
+                  <TextField
+                    name="lastName"
+                    id="lastName"
                     autoComplete="family-name"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    validation={{ required: true }}
                   />
+                  <FieldError name="lastName" />
                 </div>
               </div>
               <div className="sm:col-span-2">
@@ -148,30 +184,38 @@ const Contact = () => {
                   Email
                 </label>
                 <div className="mt-2.5">
-                  <input
-                    type="email"
+                  <TextField
                     name="email"
                     id="email"
                     autoComplete="email"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    validation={{
+                      required: true,
+                      pattern: {
+                        value: /^[^@]+@[^.]+\..+$/,
+                        message: 'Please enter a valid email address',
+                      },
+                    }}
                   />
+                  <FieldError name="email" />
                 </div>
               </div>
               <div className="sm:col-span-2">
                 <label
-                  htmlFor="phone-number"
+                  htmlFor="phoneNumber"
                   className="block text-sm font-semibold leading-6 text-gray-900"
                 >
                   Phone number
                 </label>
                 <div className="mt-2.5">
-                  <input
-                    type="tel"
-                    name="phone-number"
-                    id="phone-number"
+                  <TextField
+                    name="phoneNumber"
+                    id="phoneNumber"
                     autoComplete="tel"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    validation={{ required: true }}
                   />
+                  <FieldError name="phoneNumber" />
                 </div>
               </div>
               <div className="sm:col-span-2">
@@ -182,26 +226,24 @@ const Contact = () => {
                   Message
                 </label>
                 <div className="mt-2.5">
-                  <textarea
+                  <TextAreaField
                     name="message"
                     id="message"
-                    rows={4}
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     defaultValue={''}
+                    validation={{ required: true }}
                   />
+                  <FieldError name="message" />
                 </div>
               </div>
             </div>
             <div className="mt-8 flex justify-end">
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
+              <Submit className="rounded-md bg-blue-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Send message
-              </button>
+              </Submit>
             </div>
           </div>
-        </form>
+        </Form>
       </div>
     </div>
   )
